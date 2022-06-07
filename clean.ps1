6eb4778be7ae9a9de82873e76be70633c3a4e190
Dmitry Krayniy <krdima92@gmail.com>
2022-06-07 13:02:45 +0400

    Added pipeline

--- clean.ps1
+++ clean.ps1
@@ -1,17 +1,16 @@
 #!/usr/bin/env pwsh
 
+# Get component data and set necessary variables
 $component = Get-Content -Path "component.json" | ConvertFrom-Json
-$buildImage="$($component.registry)/$($component.name):$($component.version)-$($env:TRAVIS_BUILD_NUMBER)-build"
-$testImage="$($component.registry)/$($component.name):$($component.version)-$($env:TRAVIS_BUILD_NUMBER)-test"
-
-# Clean up build directories
-Get-ChildItem -Path "." -Include "obj" -Recurse | foreach($_) { Remove-Item -Force -Recurse $_.FullName }
-Get-ChildItem -Path "." -Include "node_modules" -Recurse | foreach($_) { Remove-Item -Force -Recurse $_.FullName }
+$testImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-test"
 
 # Remove docker images
-docker rmi $buildImage --force
 docker rmi $testImage --force
+docker rmi -f $(docker images -f "dangling=true" -q) # remove build container if build fails
 docker image prune --force
 
 # Remove existed containers
 docker ps -a | Select-String -Pattern "Exit" | foreach($_) { docker rm $_.ToString().Split(" ")[0] }
+
+# Remove unused volumes
+docker volume rm -f $(docker volume ls -f "dangling=true")
