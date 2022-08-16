71a78e0028925b5c66683c1fc7a36a96cc6e4996
Dmitry Krayniy <krdima92@gmail.com>
2022-08-16 16:55:32 +0400

    Updated pipeline

--- protogen.ps1
+++ protogen.ps1
@@ -6,23 +6,27 @@ $ErrorActionPreference = "Stop"
 # Get component data and set necessary variables
 $component = Get-Content -Path "component.json" | ConvertFrom-Json
 
-$docsImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-proto"
+$protosImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-proto"
 $container=$component.name
 
 # Remove old generate files
-if (Test-Path "lib/src/generated") {
-    Remove-Item -Path "lib/src/generated/*" -Force -Include *.dart
+if (Test-Path "$PSScriptRoot/lib/src/generated") {
+    Remove-Item -Path "$PSScriptRoot/lib/src/generated/*" -Force -Include *.dart
 }
-
-if (Test-Path "test/generated") {
-    Remove-Item -Path "test/generated/*" -Force -Include *.dart
+if (Test-Path "$PSScriptRoot/test/generated") {
+    Remove-Item -Path "$PSScriptRoot/test/generated/*" -Force -Include *.dart
 }
 
 # Build docker image
-docker build -f docker/Dockerfile.proto -t $docsImage .
+docker build -f "$PSScriptRoot/docker/Dockerfile.proto" -t $protosImage .
 
 # Create and copy compiled files, then destroy
-docker create --name $container $docsImage
-docker cp "$($container):/app/lib/src/generated" ./lib/src/
-docker cp "$($container):/app/test/generated" ./test/
+docker create --name $container $protosImage
+docker cp "$($container):/app/lib/src/generated" "$PSScriptRoot/lib/src/generated"
+docker cp "$($container):/app/test/generated" "$PSScriptRoot/test/generated"
 docker rm $container
+
+# Verify that protos folder was indeed created after generating proto files
+if (-not (Test-Path "$PSScriptRoot/lib/src/generated")) {
+    Write-Error "generated folder doesn't exist in lib/src dir. Build failed. See logs above for more information."
+}
